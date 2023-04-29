7540fc82edb3b2abedc9bed20c2cecf6a5b157a7
Rahulgoud1210 <128498644+Rahulgoud1210@users.noreply.github.com>
2023-04-30 00:07:21 +0530

    Update Plagiarism detector.py

--- Plagiarism detector.py
+++ Plagiarism detector.py
@@ -3,28 +3,28 @@ from numpy import vectorize
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-sample_files = [doc for doc in os.listdir() if doc.endswith('.txt')]
-sample_contents = [open(File).read() for File in sample_files]
+user_files = [doc for doc in os.listdir() if doc.endswith('.txt')]
+user_notes = [open(_file, encoding='utf-8').read() for _file in user_files]
 
-vectorize = lambda Text: TfidfVectorizer().fit_transform(Text).toarray()
-similarity = lambda doc1, doc2: cosine_similarity([doc1, doc2])
+def vectorize(Text): = return TfidfVectorizer().fit_transform(Text).toarray()
+def similarity(doc1, doc2): return cosine_similarity([doc1, doc2])
 
-vectors = vectorize(sample_contents)
-s_vectors = list(zip(sample_files, vectors))
+vectors = vectorize(user_notes)
+s_vectors = list(zip(user_files, vectors))
+plagiarism_results = set()
 
 def check_plagiarism():
-    results = set()
     global s_vectors
-    for sample_a, text_vector_a in s_vectors:
+    for student_a, text_vector_a in s_vectors:
         new_vectors = s_vectors.copy()
-        current_index = new_vectors.index((sample_a, text_vector_a))
+        current_index = new_vectors.index((student_a, text_vector_a))
         del new_vectors[current_index]
-        for sample_b, text_vector_b in new_vectors:
+        for student_b, text_vector_b in new_vectors:
             sim_score = similarity(text_vector_a, text_vector_b)[0][1]
-            sample_pair = sorted((sample_a, sample_b))
-            score = sample_pair[0], sample_pair[1], sim_score
-            results.add(score)
-    return results
+            student_pair = sorted((student_a, student_b))
+            score = (student_pair[0], student_pair[1], sim_score)
+            plagiarism_results.add(score)
+    return plagiarism_results
 
 for data in check_plagiarism():
     print(data)    
